95449710d9098bdc1b68c5803b44387ee6e3a740
mipangg <morganjeong119@gmail.com>
2025-12-05 17:18:35 +0900

    feat(Country): 외부 api에서 불러온 country 정보를 repository에 저장하는 기능 구현

--- src/main/java/io/mipangg/holidaykeeper/domain/country/service/CountryService.java
+++ src/main/java/io/mipangg/holidaykeeper/domain/country/service/CountryService.java
@@ -1,4 +1,32 @@
 package io.mipangg.holidaykeeper.domain.country.service;
 
+import io.mipangg.holidaykeeper.domain.country.entity.Country;
+import io.mipangg.holidaykeeper.domain.country.repository.CountryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class CountryService {
+
+    private final CountryRepository countryRepository;
+    private final ExternalCountryClient externalCountryClient;
+
+    @Transactional
+    public void syncCountries() {
+        externalCountryClient.getCountries().forEach(externalCountry -> {
+            if (countryRepository.existsByCode(externalCountry.countryCode())) {
+                return;
+            }
+
+            Country country = Country.builder()
+                    .code(externalCountry.countryCode())
+                    .name(externalCountry.name())
+                    .build();
+
+            countryRepository.save(country);
+        });
+    }
+
 }
--- src/test/java/io/mipangg/holidaykeeper/domain/country/service/CountryServiceTests.java
+++ src/test/java/io/mipangg/holidaykeeper/domain/country/service/CountryServiceTests.java
@@ -1,4 +1,49 @@
-import static org.junit.jupiter.api.Assertions.*;
-class CountryServiceTestㄴ {
-  
+package io.mipangg.holidaykeeper.domain.country.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import io.mipangg.holidaykeeper.domain.country.dto.ExternalCountryResponse;
+import io.mipangg.holidaykeeper.domain.country.repository.CountryRepository;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CountryServiceTests {
+
+    @InjectMocks
+    private CountryService countryService;
+
+    @Mock
+    private CountryRepository countryRepository;
+
+    @Mock
+    private ExternalCountryClient externalCountryClient;
+
+    @Test
+    @DisplayName("externalCountryClient로 불러온 country 정보들을 repository에 저장한다")
+    void syncCountries_success_test() {
+
+        List<ExternalCountryResponse> externalCountryResps = List.of(
+                new ExternalCountryResponse("AD", "Andorra"),
+                new ExternalCountryResponse("ZW", "Zimbabwe")
+        );
+
+        when(externalCountryClient.getCountries()).thenReturn(externalCountryResps);
+        when(countryRepository.existsByCode(anyString())).thenReturn(false);
+
+        countryService.syncCountries();
+
+        verify(countryRepository, times(2)).save(any());
+
+    }
+
 }
