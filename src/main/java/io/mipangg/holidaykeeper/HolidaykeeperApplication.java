f210272566820426c3665904eb6ef30d22e29e8c
mipangg <morganjeong119@gmail.com>
2026-01-13 00:01:19 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/io/mipangg/holidaykeeper/HolidaykeeperApplication.java
@@ -0,0 +1,13 @@
+package io.mipangg.holidaykeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidaykeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolidaykeeperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/mipangg/holidaykeeper/HolidaykeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mipangg.holidaykeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidaykeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
