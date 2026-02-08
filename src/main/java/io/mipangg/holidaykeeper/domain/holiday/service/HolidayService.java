99aa076bdcdd2649d95483d0d1b74fdf5e02898d
mipangg <morganjeong119@gmail.com>
2026-02-08 13:08:55 +0900

    feat(HolidayController): 데이터 적제 기능을 위한 컨트롤러 기능 추가

---
+++ src/main/java/io/mipangg/holidaykeeper/domain/holiday/service/HolidayService.java
@@ -0,0 +1,10 @@
+package io.mipangg.holidaykeeper.domain.holiday.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HolidayService {
+
+    public void saveHolidays() {
+    }
+}
---
+++ src/test/java/io/mipangg/holidaykeeper/domain/holiday/controller/HolidayControllerTests.java
@@ -0,0 +1,39 @@
+package io.mipangg.holidaykeeper.domain.holiday.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import io.mipangg.holidaykeeper.domain.holiday.service.HolidayService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = HolidayController.class)
+class HolidayControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private HolidayService holidayService;
+
+    @Test
+    @DisplayName("공휴일 데이터 적재 테스트")
+    void createHolidayTest() throws Exception {
+
+        mockMvc.perform(post("/holidays")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andDo(print());
+
+        verify(holidayService).saveHolidays();
+    }
+
+}
