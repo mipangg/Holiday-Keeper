037a7beb090baffda78caa6d519d8654c3064ba9
mipangg <morganjeong119@gmail.com>
2026-02-12 13:55:39 +0900

    refactor(ExternalApiClient): 외부 api 호출 예외 처리 기능 추가

--- src/main/java/io/mipangg/holidaykeeper/external/service/ExternalApiClient.java
+++ src/main/java/io/mipangg/holidaykeeper/external/service/ExternalApiClient.java
@@ -2,9 +2,12 @@ package io.mipangg.holidaykeeper.external.service;
 
 import io.mipangg.holidaykeeper.external.dto.ExternalCountryResponse;
 import io.mipangg.holidaykeeper.external.dto.ExternalHolidayResponse;
+import io.mipangg.holidaykeeper.global.exception.CustomLogicException;
+import io.mipangg.holidaykeeper.global.exception.ErrorCode;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientException;
 
 @Service
 public class ExternalApiClient {
@@ -16,21 +19,37 @@ public class ExternalApiClient {
     }
 
     public List<ExternalCountryResponse> getExternalCountries() {
-        return webClient.get()
-                .uri("/AvailableCountries")
-                .retrieve()
-                .bodyToFlux(ExternalCountryResponse.class)
-                .collectList()
-                .block();
+        try {
+            return webClient.get()
+                    .uri("/AvailableCountries")
+                    .retrieve()
+                    .bodyToFlux(ExternalCountryResponse.class)
+                    .collectList()
+                    .block();
+        } catch (WebClientException e) {
+            throw new CustomLogicException(
+                    ErrorCode.API_CALL_ERROR,
+                    "국가 목록 조회 API 호출에 실패했습니다."
+            );
+        }
+
     }
 
     public List<ExternalHolidayResponse> getExternalHolidays(int year, String countryCode) {
-        return webClient.get()
-                .uri("/PublicHolidays/{year}/{countryCode}", year, countryCode)
-                .retrieve()
-                .bodyToFlux(ExternalHolidayResponse.class)
-                .collectList()
-                .block();
+        try {
+            return webClient.get()
+                    .uri("/PublicHolidays/{year}/{countryCode}", year, countryCode)
+                    .retrieve()
+                    .bodyToFlux(ExternalHolidayResponse.class)
+                    .collectList()
+                    .block();
+        } catch (WebClientException e) {
+            throw new CustomLogicException(
+                    ErrorCode.API_CALL_ERROR,
+                    "공휴일 목록 조회 API 호출에 실패했습니다."
+            );
+        }
+
     }
 
 }
--- src/main/java/io/mipangg/holidaykeeper/global/exception/ErrorCode.java
+++ src/main/java/io/mipangg/holidaykeeper/global/exception/ErrorCode.java
@@ -23,7 +23,6 @@ public enum ErrorCode {
     /*
      * 500 INTERNAL_SERVER_ERROR: 내부 서버 오류
      */
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류입니다."),
     API_CALL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "API 호출에 실패했습니다.");
 
     private final HttpStatus httpStatus;
