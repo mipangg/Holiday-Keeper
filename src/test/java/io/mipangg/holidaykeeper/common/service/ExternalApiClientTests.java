7f6232b2f737634ac2091c7568c957134822370a
mipangg <morganjeong119@gmail.com>
2026-02-12 14:00:37 +0900

    test(ExternalApiClient): 외부 api에서 공휴일 목록을 불러오는 메서드의 테스트 코드 추가

--- src/test/java/io/mipangg/holidaykeeper/common/service/ExternalApiClientTests.java
+++ src/test/java/io/mipangg/holidaykeeper/common/service/ExternalApiClientTests.java
@@ -3,6 +3,7 @@ package io.mipangg.holidaykeeper.common.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.mipangg.holidaykeeper.external.dto.ExternalCountryResponse;
+import io.mipangg.holidaykeeper.external.dto.ExternalHolidayResponse;
 import io.mipangg.holidaykeeper.external.service.ExternalApiClient;
 import java.io.IOException;
 import java.util.List;
@@ -40,21 +41,21 @@ class ExternalApiClientTests {
 
     @Test
     @DisplayName("외부 api를 호출하여 국가 목록을 성공적으로 불러올 수 있다")
-    void getExternalCountries_success_test() {
+    void getExternalCountriesTest() {
 
         mockWebServer.enqueue(new MockResponse()
                 .setBody("""
-                         [
-                                {
-                                    "countryCode": "BR",
-                                    "name": "Brazil"
-                                },
-                                {
-                                    "countryCode": "KR",
-                                    "name": "South Korea"
-                                }
-                         ]
-                """)
+                                 [
+                                        {
+                                            "countryCode": "BR",
+                                            "name": "Brazil"
+                                        },
+                                        {
+                                            "countryCode": "KR",
+                                            "name": "South Korea"
+                                        }
+                                 ]
+                        """)
                 .addHeader("Content-Type", "application/json"));
 
         final List<ExternalCountryResponse> countries = externalApiClient.getExternalCountries();
@@ -63,6 +64,55 @@ class ExternalApiClientTests {
         assertThat(countries.getFirst().name()).isEqualTo("Brazil");
         assertThat(countries.getLast().countryCode()).isEqualTo("KR");
         assertThat(countries.getLast().name()).isEqualTo("South Korea");
+
+    }
+
+    @Test
+    @DisplayName("외부 api를 호출하여 공휴일 목록을 성공적으로 불러올 수 있다")
+    void getExternalHolidaysTest() {
+
+        mockWebServer.enqueue(new MockResponse()
+                .setBody("""
+                                [
+                                        {
+                                            "date": "2026-01-01",
+                                            "localName": "New Year's Day",
+                                            "name": "New Year's Day",
+                                            "countryCode": "CA",
+                                            "fixed": false,
+                                            "global": true,
+                                            "counties": null,
+                                            "launchYear": null,
+                                            "types": [
+                                               "Public"
+                                            ]
+                                        },
+                                        {
+                                             "date": "2026-02-16",
+                                             "localName": "Louis Riel Day",
+                                             "name": "Louis Riel Day",
+                                             "countryCode": "CA",
+                                             "fixed": false,
+                                             "global": false,
+                                             "counties": [
+                                             "CA-MB"
+                                             ],
+                                             "launchYear": null,
+                                             "types": [
+                                                "Public"
+                                             ]
+                                        }
+                                ]
+                        """)
+                .addHeader("Content-Type", "application/json"));
+
+        final List<ExternalHolidayResponse> holidays =
+                externalApiClient.getExternalHolidays(2026, "CA");
+
+        assertThat(holidays.getFirst().date()).isEqualTo("2026-01-01");
+        assertThat(holidays.getFirst().localName()).isEqualTo("New Year's Day");
+        assertThat(holidays.getLast().date()).isEqualTo("2026-02-16");
+        assertThat(holidays.getLast().localName()).isEqualTo("Louis Riel Day");
 
     }
 
